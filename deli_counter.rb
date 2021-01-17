b76c23ad44261ca55c178e6b69b80431a6cced95
Abdelrahman Elsirafy <rahman2408@gmail.com>
2021-01-17 05:27:05 -0500

    commit tree 3a6a167e24da20ddd5b4747f6d2382dfbeb39f0d

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,45 @@
-# Write your code here.
+require 'pry'
+
+katz_deli = []
+other_deli = ["Logan", "Avi", "Spencer"]
+another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey" ]
+
+
+def line(array)
+index_name = ""
+     if array.length == 0
+        puts   "The line is currently empty."
+    else
+        array.each.with_index {|name, index| index_name. << " #{index+1}. #{name}"}
+        
+        puts "The line is currently:#{index_name}"
+    end
+
+ end
+
+
+       
+ def take_a_number(array,string)
+ 
+ 
+    array << "#{string}"
+   
+ 
+             puts "Welcome, #{string}. You are number #{array.index("#{string}")+1} in line."
+
+    end
+
+
+
+
+ def now_serving (array)
+
+    if array.length > 0
+       puts "Currently serving #{array.first}."
+        array.shift
+    else  
+     puts "There is nobody waiting to be served!"
+   
+    end
+    
+end
